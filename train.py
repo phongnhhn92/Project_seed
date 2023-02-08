8422191d73b84859ec6c741c01fd83ac0ca3bae4
phong <phongnhhn92@gmail.com>
2023-02-08 20:40:02 +0200

    Revert "first commit"
    This reverts commit 99f9372cd044aa63e14bc312a220d141e9a4fc53.

--- train.py
+++ train.py
@@ -1,17 +1,8 @@
 import torch, os
-import numpy as np
 from torch.utils.data import DataLoader
 from opt import config_parser
 
-# model
-from model import Unet, GaussianDiffusion
-
-# dataset
-from torchvision import datasets
-import torchvision.transforms.transforms as T
-from torchvision.utils import save_image
 # optimizer, scheduler, visualization
-from utils import *
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
 # pytorch-lightning
@@ -21,87 +12,35 @@ from pytorch_lightning import LightningModule, Trainer, loggers
 
 
 class Project_name(LightningModule):
-    def __init__(self, hparams):
-        super().__init__()
-        self.save_hyperparameters(hparams)
-        self.model = Unet(
-                dim = 64,
-                dim_mults = (1, 2, 4, 8)
-        )
-
-        self.diffusion = GaussianDiffusion(
-                self.model,
-                image_size = 32,
-                timesteps = self.hparams.num_step,   # number of steps
-                sampling_timesteps = self.hparams.num_step // 4,
-                loss_type = 'l1'    # L1 or L2
-        )
+    def __init__(self, args):
+        super(Project_name,self).__init__()
+    
     # def decode_batch():
     #     return None
     
     def forward(self):
-        return None
+        return 
 
     def prepare_data(self):
-        transforms = T.Compose([T.RandomHorizontalFlip(), T.ToTensor()])
-        self.train_dataset = datasets.CIFAR100("./data/CIFAR", download=True, train=True, transform=transforms)
-        # self.test_dataset = datasets.CIFAR100("./data/CIFAR", download=True, train=False, transform=transforms)
-        # indices = list(range(len(self.train_dataset)))
-        # np.random.shuffle(indices)
-
-        # split = int(np.floor(0.2* len(self.train_dataset)))
-        # self.train_sample = SubsetRandomSampler(indices[:split])
-        # self.test_sample = SubsetRandomSampler(indices[split:])
+        return 
     
     def configure_optimizers(self):
-        self.optimizer = get_optimizer(self.hparams, [self.diffusion])
-        scheduler = get_scheduler(self.hparams, self.optimizer)
-
-        return [self.optimizer], [scheduler]
+        return super().configure_optimizers()
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, num_workers=4, batch_size=self.hparams.batchSize, shuffle=True)
+        return super().train_dataloader()
 
-    # def val_dataloader(self):
-    #     return DataLoader(self.train_dataset, shuffle=False, num_workers=1, batch_size=1)
-        
-    # def test_dataloader(self):
-    #     return DataLoader(self.test_dataloader, num_workers=1, batch_size=1, sampler=self.test_sample)
+    def val_dataloader(self):
+        return super().val_dataloader()
 
-    def training_step(self, batch, batch_nb):
-        learning_rate = self.optimizer.param_groups[0]['lr']
-        self.log('lr', learning_rate, prog_bar=True, logger=True)
-        imgs = batch[0]
-        B,C,H,W = imgs.shape
+    def training_step(self, *args, **kwargs):
+        return super().training_step(*args, **kwargs)
 
-        if C != 3:
-            imgs = torch.repeat_interleave(imgs, repeats=3, dim=1)
+    def validation_step(self, *args, **kwargs):
+        return super().validation_step(*args, **kwargs)
 
-        loss = self.diffusion(imgs)
-        self.log('train_loss', loss, prog_bar=True, logger=True, on_epoch=True, on_step=False)
-
-        if batch_nb % self.hparams.num_vis_step == 0:
-            sampled_images = self.diffusion.sample(batch_size = self.hparams.num_vis)
-            saving_folder = os.path.join(self.hparams.sample_dir, f'{args.expname}',f'{self.logger.version}')
-            if os.path.exists(saving_folder) is not True:
-                os.makedirs(os.path.join(saving_folder,'generated'))
-
-            save_image(sampled_images, os.path.join(saving_folder,'generated', f'{self.current_epoch:04}_{batch_nb:04}.png'))
-
-            if self.hparams.save_imgs:
-                if os.path.exists(os.path.join(saving_folder,'GT')) is not True:
-                    os.makedirs(os.path.join(saving_folder,'GT'))
-                save_image(imgs, os.path.join(saving_folder,'GT', f'{self.current_epoch:04}_{batch_nb:04}.png'))
-
-        return loss
-
-    # def validation_step(self, batch, batch_nb):
-    #     log = {}
-    #     return log
-        
-
-    # def validation_epoch_end(self, outputs):
-    #     return None
+    def validation_epoch_end(self, outputs):
+        return super().validation_epoch_end(outputs)
     
     def save_ckpt(self):
         print('saved!')
@@ -115,7 +54,7 @@ if __name__ == '__main__':
 
     checkpoint_callback = ModelCheckpoint(dirpath= f'{args.log_dir}/{args.expname}/ckpts/',
                                           filename= '{epoch:02d}',
-                                          monitor='train_loss',
+                                          monitor='val/PSNR',
                                           mode='max',
                                           save_top_k=3)
     bar = TQDMProgressBar(refresh_rate=1000 if args.num_gpus > 1 else 1)
@@ -129,10 +68,9 @@ if __name__ == '__main__':
                       logger=logger,
                       enable_model_summary=False,
                       accelerator="gpu" if args.num_gpus >= 1 else "cpu",
-                      devices=args.num_gpus if args.num_gpus is not None else 0,   
-                      gradient_clip_val=args.grad_clip,                                   
-                      #num_sanity_val_steps=1,
-                      #check_val_every_n_epoch = max(args.num_epochs//args.N_vis,1),
+                      devices=args.num_gpus if args.num_gpus is not None else 0,                                       
+                      num_sanity_val_steps=1,
+                      check_val_every_n_epoch = max(args.num_epochs//args.N_vis,1),
                       benchmark=True,)
 
     trainer.fit(system)
